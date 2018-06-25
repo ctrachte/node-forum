467636fedb9be6eefbbfa5d93f294135e69812fe
Caleb Trachte <cetrachte@hotmail.com>
2018-06-25 12:23:19 -0500

    add routes, routing config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
+const routeConfig = require("./config/route-config.js");
 
-app.use("/", (req, res, next) => {
-  res.send("Welcome to Bloccit")
-});
-
+routeConfig.init(app);
 module.exports = app;
