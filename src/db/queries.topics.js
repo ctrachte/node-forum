67e23af2d1a7b02538b6bd48d8e7ef9b1e79d7de
Caleb Trachte <cetrachte@hotmail.com>
2018-07-05 11:29:04 -0500

    add query for new topics

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -2,13 +2,22 @@ const Topic = require("./models").Topic;
 
 module.exports = {
 
-//#1
   getAllTopics(callback){
     return Topic.all()
-
-//#2
     .then((topics) => {
       callback(null, topics);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  addTopic(newTopic, callback){
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
     })
     .catch((err) => {
       callback(err);
