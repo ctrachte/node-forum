698e0d0e38d0dd23543b201ed5d797ba2f67d50c
Caleb Trachte <cetrachte@hotmail.com>
2018-07-09 13:08:58 -0500

    add post spec, migration, models update topic model and migration to include post and null checks

---
+++ src/db/models/post.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Post = sequelize.define('Post', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {});
+  Post.associate = function(models) {
+    Post.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+  };
+  return Post;
+};
