2200bdffcf12cda6a9095df60f44f8116296c227
Caleb Trachte <cetrachte@hotmail.com>
2018-07-11 13:20:27 -0500

    add new spec, controller action, query, route and partial view for show posts

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -14,7 +14,6 @@ describe("routes : posts", () => {
 
     sequelize.sync({force: true}).then((res) => {
 
-//#1
       Topic.create({
         title: "Winter Games",
         description: "Post your Winter Games stories."
@@ -48,6 +47,47 @@ describe("routes : posts", () => {
         done();
       });
     });
+
+  });
+  describe("POST /topics/:topicId/posts/create", () => {
+
+     it("should create a new post and redirect", (done) => {
+        const options = {
+          url: `${base}/${this.topic.id}/posts/create`,
+          form: {
+            title: "Watching snow melt",
+            body: "Without a doubt my favoriting things to do besides watching paint dry!"
+          }
+        };
+        request.post(options,
+          (err, res, body) => {
+
+            Post.findOne({where: {title: "Watching snow melt"}})
+            .then((post) => {
+              expect(post).not.toBeNull();
+              expect(post.title).toBe("Watching snow melt");
+              expect(post.body).toBe("Without a doubt my favoriting things to do besides watching paint dry!");
+              expect(post.topicId).not.toBeNull();
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          }
+        );
+      });
+
+   });
+  describe("GET /topics/:topicId/posts/:id", () => {
+
+   it("should render a view with the selected post", (done) => {
+     request.get(`${base}/${this.topic.id}/posts/${this.post.id}`, (err, res, body) => {
+       expect(err).toBeNull();
+       expect(body).toContain("Snowball Fighting");
+       done();
+     });
+   });
 
   });
 });
