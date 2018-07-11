edcb6e46f3b6f326b77324e6b662cb160c4ab7f5
Caleb Trachte <cetrachte@hotmail.com>
2018-07-11 12:47:23 -0500

    add controller method, new post partial view, links

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const postController = require("../controllers/postController")
+router.get("/topics/:topicId/posts/new", postController.new);
 
 module.exports = router;
