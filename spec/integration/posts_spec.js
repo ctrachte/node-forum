46f23a7018dcc2b66ee9316b8aace933d792a64d
Caleb Trachte <cetrachte@hotmail.com>
2018-07-11 13:29:47 -0500

    add delete controller action, query, partial view, spec

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -90,4 +90,23 @@ describe("routes : posts", () => {
    });
 
   });
+  describe("POST /topics/:topicId/posts/:id/destroy", () => {
+
+    it("should delete the post with the associated ID", (done) => {
+
+      expect(this.post.id).toBe(1);
+
+      request.post(`${base}/${this.topic.id}/posts/${this.post.id}/destroy`, (err, res, body) => {
+
+        Post.findById(1)
+        .then((post) => {
+          expect(err).toBeNull();
+          expect(post).toBeNull();
+          done();
+        })
+      });
+
+    });
+
+  });
 });
--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -19,5 +19,16 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+  deletePost(id, callback){
+    return Post.destroy({
+      where: { id }
+    })
+    .then((deletedRecordsCount) => {
+      callback(null, deletedRecordsCount);
+    })
+    .catch((err) => {
+      callback(err);
+    })
   }
 }
