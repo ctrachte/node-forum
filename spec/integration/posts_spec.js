d8d1f842dbe80597ed947f5180693009b09e661a
Caleb Trachte <cetrachte@hotmail.com>
2018-07-12 13:22:46 -0500

    add boostrap modal to view errors on post views and index add validation error message partial views add cookieSecret to main config fix bug in posts_spec update method

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -161,9 +161,10 @@ describe("routes : posts", () => {
 
      it("should update the post with the given values", (done) => {
          const options = {
-           url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
+           url: `${base}/${this.post.topicId}/posts/${this.post.id}/update`,
            form: {
-             title: "Snowman Building Competition"
+             title: "Snowman Building Competition",
+             body: "I love watching them melt quickly."
            }
          };
          request.post(options,
