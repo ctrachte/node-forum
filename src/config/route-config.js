b05b0305db780c8219dac7ffd9377d369934e09c
Caleb Trachte <cetrachte@hotmail.com>
2018-07-19 11:22:43 -0500

    add route, controller, query, validation, policies, partial views, and specs for comments

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,12 +5,16 @@ module.exports = {
     const topicRoutes = require("../routes/topics");
     const postRoutes = require("../routes/posts");
     const userRoutes = require("../routes/users");
+    const commentRoutes = require("../routes/comments");
+
     if(process.env.NODE_ENV == "test") {
       mockAuth.fakeIt(app);
     };
+    
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(postRoutes);
     app.use(userRoutes);
+    app.use(commentRoutes);
   }
 }
